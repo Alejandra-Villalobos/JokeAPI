3a2849841fda452a3d27cd0cfb4f6bddbb8c73bc
alejandra-villalobos <00132820@uca.edu.sv>
2023-05-05 20:49:31 -0600

    ✨ Added: Style on joke container

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let jokeP1 = document.querySelector('#joke-p1');
 let jokeP2 = document.querySelector('#joke-p2');
+let jokeDiv = document.querySelector('#joke-div');
 let generate = document.querySelector('#generate');
 let es = document.querySelector('#es');
 let en = document.querySelector('#en');
@@ -8,10 +9,14 @@ let url = 'https://v2.jokeapi.dev/joke/Any?'
 
 es.addEventListener('click', () => {
     url = 'https://v2.jokeapi.dev/joke/Any?lang=es';
+    es.style.backgroundColor = 'navy';
+    en.style.backgroundColor = 'inherit';
 });
 
 en.addEventListener('click', () => {
     url = 'https://v2.jokeapi.dev/joke/Any?';
+    en.style.backgroundColor = 'navy';
+    es.style.backgroundColor = 'inherit';
 });
 
 generate.addEventListener('click', () =>{
@@ -25,9 +30,11 @@ generate.addEventListener('click', () =>{
                 jokeP1.innerHTML = `- ${JSON.stringify(json.setup)}`;
                 jokeP2.innerHTML = `+ ${JSON.stringify(json.delivery)}`;
             }
+            jokeDiv.style.display = 'block';
         })
         .catch(err => {
             console.log("Error!", err);
             jokeP1.innerHTML = `${err}`;
-        });
+        })
+        .finally(() => jokeDiv.style.display = 'block');
 })
